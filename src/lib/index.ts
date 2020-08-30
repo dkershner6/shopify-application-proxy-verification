5e73eda89cc13d47836eeade295f23885f1534f8
Derek Kershner <dkershner@cbtnuggets.com>
2020-08-29 17:48:49 -0700

    Fixed test cmd in github action

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,23 @@
 import crypto from 'crypto';
 
+/**
+ * This function will return true on a valid shopify application proxy request.
+ * @param parsedQueryString An object containing the complete query string.
+ * @param shopifySecret The shopify secret for your app.
+ * @returns boolean
+ */
 const verifyAppProxyHmac = (
     parsedQueryString: Record<string, string | string[]>,
     shopifySecret: string
 ): boolean => {
+    if (
+        !parsedQueryString ||
+        typeof parsedQueryString !== 'object' ||
+        !shopifySecret
+    ) {
+        return false;
+    }
+
     const { signature, ...otherQueryParams } = parsedQueryString;
 
     const input = Object.keys(otherQueryParams)
